3c0e80e94f212a45b9312751addf9793dbadaa49
emapiric <emilija.piric@dericon.de>
2021-08-07 17:18:47 +0200

    added crossorigin to userepository

--- src/main/java/com/carpool/repository/UserRepository.java
+++ src/main/java/com/carpool/repository/UserRepository.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:8100")
 @RepositoryRestResource(collectionResourceRel = "user", path="user")
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 	Optional<UserEntity> findByEmail(String email);
