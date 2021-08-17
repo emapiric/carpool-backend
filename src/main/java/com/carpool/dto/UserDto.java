24dfccb22235edee9861b9f3829b22a0e6f30b8b
emapiric <emilija.piric@dericon.de>
2021-08-17 16:17:49 +0200

    Removed comment

--- src/main/java/com/carpool/dto/UserDto.java
+++ src/main/java/com/carpool/dto/UserDto.java
@@ -18,7 +18,7 @@ public class UserDto implements MyDto {
     @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
     private String email;
     @NotNull
-   // @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,}$")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])(?=\\S+$).{8,}$")
     private String password;
     @Size(min = 3)
     private String fullName;
