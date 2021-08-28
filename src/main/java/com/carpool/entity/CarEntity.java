080d3356324fc415c1e1c6dee7a21b36f5e88bd5
miapecelj <miapecelj16@gmail.com>
2021-08-28 15:31:13 +0200

    Added relationship User-Car

--- src/main/java/com/carpool/entity/CarEntity.java
+++ src/main/java/com/carpool/entity/CarEntity.java
@@ -1,105 +1,124 @@
 package com.carpool.entity;
 
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
 @Entity
-@Table(name="car")
+@Table(name = "car")
 public class CarEntity implements MyEntity {
-    @Id
-    @Column(name = "plate_number")
-    private String id;
-    private String manufacturer;
-    private String model;
-    private String type;
-    @Column(name = "year_of_manufacturing")
-    private LocalDate yearOfManufacturing = LocalDate.now();
-    private String color;
+	@Id
+	@Column(name = "plate_number")
+	private String id;
+	private String manufacturer;
+	private String model;
+	private String type;
+	@Column(name = "year_of_manufacturing")
+	private LocalDate yearOfManufacturing = LocalDate.now();
+	private String color;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner_id", referencedColumnName = "id")
+	private UserEntity owner;
 
-    public CarEntity(String id, String manufacturer, String model, String type, LocalDate yearOfManufacturing, String color) {
-        this.id = id;
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.type = type;
-        this.yearOfManufacturing = yearOfManufacturing;
-        this.color = color;
-    }
+	public CarEntity(String id, String manufacturer, String model, String type, LocalDate yearOfManufacturing,
+			String color, UserEntity owner) {
+		super();
+		this.id = id;
+		this.manufacturer = manufacturer;
+		this.model = model;
+		this.type = type;
+		this.yearOfManufacturing = yearOfManufacturing;
+		this.color = color;
+		this.owner = owner;
+	}
 
-    public CarEntity() {
-    }
+	public UserEntity getOwner() {
+		return owner;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public void setOwner(UserEntity owner) {
+		this.owner = owner;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public CarEntity() {
+	}
 
-    public String getManufacturer() {
-        return manufacturer;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getModel() {
-        return model;
-    }
+	public String getManufacturer() {
+		return manufacturer;
+	}
 
-    public void setModel(String model) {
-        this.model = model;
-    }
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getModel() {
+		return model;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setModel(String model) {
+		this.model = model;
+	}
 
-    public LocalDate getYearOfManufacturing() {
-        return yearOfManufacturing;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public void setYearOfManufacturing(LocalDate yearOfManufacturing) {
-        this.yearOfManufacturing = yearOfManufacturing;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public String getColor() {
-        return color;
-    }
+	public LocalDate getYearOfManufacturing() {
+		return yearOfManufacturing;
+	}
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+	public void setYearOfManufacturing(LocalDate yearOfManufacturing) {
+		this.yearOfManufacturing = yearOfManufacturing;
+	}
 
+	public String getColor() {
+		return color;
+	}
 
-    @Override
-    public String toString() {
-        return "CarEntity{" +
-                "id=" + id +
-                ", manufacturer='" + manufacturer + '\'' +
-                ", model='" + model + '\'' +
-                ", type='" + type + '\'' +
-                ", yearOfManufacturing=" + yearOfManufacturing +
-                ", color='" + color+
-                '}';
-    }
+	public void setColor(String color) {
+		this.color = color;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CarEntity)) return false;
-        CarEntity carEntity = (CarEntity) o;
-        return id == carEntity.id && Objects.equals(manufacturer, carEntity.manufacturer) && Objects.equals(model, carEntity.model) && Objects.equals(type, carEntity.type) && Objects.equals(yearOfManufacturing, carEntity.yearOfManufacturing) && Objects.equals(color, carEntity.color);
-    }
+	@Override
+	public String toString() {
+		return "CarEntity{" + "id=" + id + ", manufacturer='" + manufacturer + '\'' + ", model='" + model + '\''
+				+ ", type='" + type + '\'' + ", yearOfManufacturing=" + yearOfManufacturing + ", color='" + color + '}';
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, manufacturer, model, type, yearOfManufacturing, color);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof CarEntity))
+			return false;
+		CarEntity carEntity = (CarEntity) o;
+		return id == carEntity.id && Objects.equals(manufacturer, carEntity.manufacturer)
+				&& Objects.equals(model, carEntity.model) && Objects.equals(type, carEntity.type)
+				&& Objects.equals(yearOfManufacturing, carEntity.yearOfManufacturing)
+				&& Objects.equals(color, carEntity.color);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, manufacturer, model, type, yearOfManufacturing, color);
+	}
 }
--- src/main/java/com/carpool/entity/UserEntity.java
+++ src/main/java/com/carpool/entity/UserEntity.java
@@ -8,11 +8,10 @@ import java.util.Objects;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -20,198 +19,195 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "user")
 public class UserEntity implements MyEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String username;
-    private String email;
-    private String password;
-    @Column(name = "full_name")
-    private String fullName;
-    private String phone;
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "plate_number")
-    private CarEntity car;
-    @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<RatingEntity> ratings = new ArrayList<RatingEntity>();
-    @OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, orphanRemoval = true)
-    private List<TakenRideEntity> takenRides = new ArrayList<TakenRideEntity>();
-    @OneToMany(mappedBy = "driver", cascade = CascadeType.MERGE, orphanRemoval = true)
-    private List<RideEntity> drivenRides = new ArrayList<RideEntity>();
-    private Boolean enabled;
-    private String confirmationToken;
-    private String resetPasswordToken;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String username;
+	private String email;
+	private String password;
+	@Column(name = "full_name")
+	private String fullName;
+	private String phone;
+	@OneToOne(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = true)
+	private CarEntity car;
+	@OneToMany(mappedBy = "driver", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<RatingEntity> ratings = new ArrayList<RatingEntity>();
+	@OneToMany(mappedBy = "user", cascade = CascadeType.MERGE, orphanRemoval = true)
+	private List<TakenRideEntity> takenRides = new ArrayList<TakenRideEntity>();
+	@OneToMany(mappedBy = "driver", cascade = CascadeType.MERGE, orphanRemoval = true)
+	private List<RideEntity> drivenRides = new ArrayList<RideEntity>();
+	private Boolean enabled;
+	private String confirmationToken;
+	private String resetPasswordToken;
 
-    public UserEntity(Long id, String username, String email, String password, String fullName, String phone, CarEntity car,
-                      List<RatingEntity> ratings, List<TakenRideEntity> takenRides, List<RideEntity> drivenRides, Boolean enabled, String confirmationToken, String forgotPasswordToken) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.fullName = fullName;
-        this.phone = phone;
-        this.car = car;
-        this.ratings = ratings;
-        this.takenRides = takenRides;
-        this.drivenRides = drivenRides;
-        this.enabled = enabled;
-        this.confirmationToken = confirmationToken;
-        this.resetPasswordToken = forgotPasswordToken;
-    }
+	public UserEntity(Long id, String username, String email, String password, String fullName, String phone,
+			CarEntity car, List<RatingEntity> ratings, List<TakenRideEntity> takenRides, List<RideEntity> drivenRides,
+			Boolean enabled, String confirmationToken, String forgotPasswordToken) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.fullName = fullName;
+		this.phone = phone;
+		this.car = car;
+		this.ratings = ratings;
+		this.takenRides = takenRides;
+		this.drivenRides = drivenRides;
+		this.enabled = enabled;
+		this.confirmationToken = confirmationToken;
+		this.resetPasswordToken = forgotPasswordToken;
+	}
 
-    public UserEntity() {
-    }
+	public UserEntity() {
+	}
 
-    public String getConfirmationToken() {
-        return confirmationToken;
-    }
+	public String getConfirmationToken() {
+		return confirmationToken;
+	}
 
-    public void setConfirmationToken(String confirmationToken) {
-        this.confirmationToken = confirmationToken;
-    }
+	public void setConfirmationToken(String confirmationToken) {
+		this.confirmationToken = confirmationToken;
+	}
 
-    public String getForgotPasswordToken() {
-        return resetPasswordToken;
-    }
+	public String getForgotPasswordToken() {
+		return resetPasswordToken;
+	}
 
-    public void setForgotPasswordToken(String forgotPasswordToken) {
-        this.resetPasswordToken = forgotPasswordToken;
-    }
+	public void setForgotPasswordToken(String forgotPasswordToken) {
+		this.resetPasswordToken = forgotPasswordToken;
+	}
 
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Boolean getEnabled() {
+		return enabled;
+	}
 
-    public Boolean getEnabled() {
-        return enabled;
-    }
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
 
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getFullName() {
+		return fullName;
+	}
 
-    public String getFullName() {
-        return fullName;
-    }
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public CarEntity getCar() {
+		return car;
+	}
 
-    public CarEntity getCar() {
-        return car;
-    }
+	public void setCar(CarEntity car) {
+		this.car = car;
+	}
 
-    public void setCar(CarEntity car) {
-        this.car = car;
-    }
+	public List<RatingEntity> getRatings() {
+		return ratings;
+	}
 
-    public List<RatingEntity> getRatings() {
-        return ratings;
-    }
+	public void setRatings(List<RatingEntity> ratings) {
+		this.ratings = ratings;
+	}
 
-    public void setRatings(List<RatingEntity> ratings) {
-        this.ratings = ratings;
-    }
+	public List<TakenRideEntity> getTakenRides() {
+		return takenRides;
+	}
 
-    public List<TakenRideEntity> getTakenRides() {
-        return takenRides;
-    }
+	public void setTakenRides(List<TakenRideEntity> takenRides) {
+		this.takenRides = takenRides;
+	}
 
-    public void setTakenRides(List<TakenRideEntity> takenRides) {
-        this.takenRides = takenRides;
-    }
+	public List<RideEntity> getDrivenRides() {
+		return drivenRides;
+	}
 
-    public List<RideEntity> getDrivenRides() {
-        return drivenRides;
-    }
+	public void setDrivenRides(List<RideEntity> drivenRides) {
+		this.drivenRides = drivenRides;
+	}
 
-    public void setDrivenRides(List<RideEntity> drivenRides) {
-        this.drivenRides = drivenRides;
-    }
+	public void addTakenRide(TakenRideEntity takenRide) throws Exception {
+		if (takenRides.contains(takenRide)) {
+			throw new Exception("Taken ride already exist");
+		} else {
+			takenRides.add(takenRide);
+		}
+	}
 
-    public void addTakenRide(TakenRideEntity takenRide) throws Exception {
-        if (takenRides.contains(takenRide)) {
-            throw new Exception("Taken ride already exist");
-        } else {
-            takenRides.add(takenRide);
-        }
-    }
+	public void removeTakenRide(RideEntity ride) {
+		for (Iterator<TakenRideEntity> iterator = takenRides.iterator(); iterator.hasNext();) {
+			TakenRideEntity takenRideEntity = iterator.next();
 
-    public void removeTakenRide(RideEntity ride) {
-        for (Iterator<TakenRideEntity> iterator = takenRides.iterator(); iterator.hasNext(); ) {
-            TakenRideEntity takenRideEntity = iterator.next();
+			if (takenRideEntity.getUser().equals(this) && takenRideEntity.getRide().equals(ride)) {
+				iterator.remove();
+				takenRideEntity.getRide().getPassengers().remove(takenRideEntity);
+				takenRideEntity.setUser(null);
+				takenRideEntity.setRide(null);
+			}
+		}
+	}
 
-            if (takenRideEntity.getUser().equals(this) && takenRideEntity.getRide().equals(ride)) {
-                iterator.remove();
-                takenRideEntity.getRide().getPassengers().remove(takenRideEntity);
-                takenRideEntity.setUser(null);
-                takenRideEntity.setRide(null);
-            }
-        }
-    }
+	@Override
+	public String toString() {
+		return "UserEntity{" + "id=" + id + ", username='" + username + '\'' + ", email='" + email + '\''
+				+ ", password='" + password + '\'' + ", fullName='" + fullName + '\'' + ", phone='" + phone + '\''
+				+ ", car=" + car + '}';
+	}
 
-    @Override
-    public String toString() {
-        return "UserEntity{" + "id=" + id + ", username='" + username + '\'' + ", email='" + email + '\''
-                + ", password='" + password + '\'' + ", fullName='" + fullName + '\'' + ", phone='" + phone + '\''
-                + ", car=" + car + '}';
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof UserEntity))
+			return false;
+		UserEntity that = (UserEntity) o;
+		return id == that.id && Objects.equals(username, that.username) && Objects.equals(email, that.email)
+				&& Objects.equals(password, that.password) && Objects.equals(fullName, that.fullName)
+				&& Objects.equals(phone, that.phone) && Objects.equals(car, that.car)
+				&& Objects.equals(drivenRides, that.drivenRides);
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof UserEntity))
-            return false;
-        UserEntity that = (UserEntity) o;
-        return id == that.id && Objects.equals(username, that.username) && Objects.equals(email, that.email)
-                && Objects.equals(password, that.password) && Objects.equals(fullName, that.fullName)
-                && Objects.equals(phone, that.phone)
-                && Objects.equals(car, that.car)
-                && Objects.equals(drivenRides, that.drivenRides);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username, email, password, fullName, phone, car,
-                takenRides, drivenRides);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, username, email, password, fullName, phone, car, takenRides, drivenRides);
+	}
 }
